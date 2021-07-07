e6700e582c705e683eb62260df6ad9c91b790b4a
Olga Poddubskaya <ocoolane@gmail.com>
2021-07-07 11:26:40 +0800

    changes for dropdown menu

--- app/blocks/dropdown/dropdown.js
+++ app/blocks/dropdown/dropdown.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const modalWindow = document.querySelector('.dropdown');
-    const modalWindowOpenBtn = document.querySelector('.header__menu-icon');
+    const dropdownMenu = document.querySelector('.dropdown');
+    const dropdownMenuItems = document.querySelectorAll('.dropdown__nav-item');
+    const dropdownOpenBtn = document.querySelector('.header__menu-icon');
+    const dropdownActiveClass = 'dropdown_state_active';
 
-
-    modalWindowOpenBtn.onclick = () => {
-        modalWindow.classList.toggle('dropdown_state_active');
+    const toggleDropdown = (dropdownMenu, activeClass) => {
+        dropdownMenu.classList.toggle(activeClass);
     }
+
+    dropdownOpenBtn.onclick = () => {
+        toggleDropdown(dropdownMenu, dropdownActiveClass);
+    }
+
+    dropdownMenuItems.forEach((dropdownMenuItem) => {
+        dropdownMenuItem.addEventListener('click', (e) => {
+            toggleDropdown(dropdownMenu, dropdownActiveClass);
+        })
+    })
 })
